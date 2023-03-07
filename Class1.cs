063d08fb1344b951fb648ef7f942950edc54aad6
marumaru99 <102706594+marumaru99@users.noreply.github.com>
2023-03-07 21:00:37 +0900

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        //C#의 데이터 타입
+        //Bool(논리형) : 결과에 대한 표현 (true/false)
+        bool isWalk = true;
+        //Numeric(숫자에 대한 표현)
+        //정수
+        short sh = -32768;
+        int i = 214748347;
+        long l = 214748347L; //long은 운영체제에 따라 크기가 변환되는 자료형
+        //실수
+        float f = 0.123456789f;//소수점 9자리 까지는 오차없이 표현
+        double d = 0.123456789012345678; //double타입은 약 소수점 18자리 까지는 오차없이 표현
+        decimal de = 0.12346M; //고정 소수점이라는 방식으로 소수를 표현
+
+        //실수 계산방식
+        //1. 부동 소수점 : 작은 메모리에 큰 소수를 저장할 수 있음. 정확성이 떨어짐
+        //2. 고정 소수점 : 연산 속도가 빠르고 정확성이 높음. 메모리가 많이 필요함
+
+        //문자/문장
+        char ch = 'ㅇ'; //문자에 대한 표현 시 ''로 작업을 진행. 영문자, 특수문자, 한글 가능
+        string s = "문장"; //C++때부터 사용하던 문자열 객체 string
+
+        static void Main(string[] args)
+        {
+            #region 시간
+            /*//시간에 대한 클래스 DateTime이 추가됨
+            DateTime dateTime = new DateTime(2023, 03, 16, 11, 0, 0);
+            while(true) //무제한 반복
+            {
+                dateTime = dateTime.AddSeconds(1);
+                Console.WriteLine(dateTime.ToString());
+                Thread.Sleep(1000);//쓰레드 환경에서 1초간 정지
+                //쓰레드(Thread) : 프로세스 내에서 실행되는 흐름의 단위
+                Console.Clear(); //콘솔창 화면 지우기
+            }
+            //ToString()을 통해 객체의 값을 문자로 표현하는 기능이 추가됨*/
+            #endregion
+
+            int MaxInteger = int.MaxValue;
+            //uint타입은 unsigned int를 의미하며 -값을 계산하지 않고 
+            //그 수치만큼 +해 양의 정수를 더 많이 표현하기 위함
+            int MinInteger = int.MinValue;
+
+            Console.WriteLine($"Int의 최대값 = {MaxInteger}");
+            Console.WriteLine($"Int의 최소값 = {MinInteger}");
+
+            string s = null; //null이 추가됨
+            //C, C++에서 데이터가 없다라는 의미로 NULL을 사용하는데 
+            //C#에서는 소문자 null을 사용
+
+            //null을 가질 수 있는 데이터 타입은 Reference타입.
+            //Value타입일 경우 null을 가질 수 없음
+            //int a = null; 불가능
+
+            //Nullable Type
+            //일반적으로 Value타임은 null을 가지지 못하기 때문에 이를 위해 만들어진 표현
+            //자료형? 으로 표현
+            int? i = null;
+            i = 100; //초기화 진행하면 값으로 적용
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//해당 지시자들은 과거에 필요했으나 현재에는 자동으로 처리가 되어 필요 없어짐
+//그래서 투명하게 처리됨. 존재하는 이유는 이전 버전에서 사용하던 코드와의 호환성 문제
+
+//using을 이용해 특정 위치에 있는 값들을 사용할 수 있게 해줌
+
+
+//C#은 namespace를 통해 코드를 구분해 작업 진행
+//유니티에서도 가능하지만 따로 네임스페이스를 처리 안하는 경우도 있음
+namespace ConsoleApp1
+{
+    //클래스 단위로 프로그램 표현
+    //기존의 C++의 경우는 메인 함수를 만들고 클래스를 설계해 호출하는 식으로 설계
+    //C#은 클래스 단위로 프로그램이 굴러가기 때문에 클래스 내부에 메인 함수를 작성하는 식으로 설계
+    internal class Program
+    {
+        //모든 c#프로그램은 메인 함수를 가지고 있어야 함
+        //메인 함수는 임의의 클래스 안에 존재하게 되며, 프로그램 기준으로 1개만 존재해야함
+        //파일의 이름이 클래스 단위로 표현되기 때문에 보통 클래스의 이름을 가진 클래스는
+        //메인 함수를 가진 클래스로 설계되는 경우가 대부분.
+        //메인 함수는 string[]을 메소드의 인자로 가지고 있으며 static으로 선언
+        //string[] args에는 프로그램을 실행할 때 넣어준 문자열들이 들어가게 됨
+
+        //static이 붙은 함수를 정적 메소드 또는 클래스 메소드라고 부르며, 클래스로부터 객체를 따로 생성하지 않고
+        //직접 클래스 그 자체로 호출이 진행되는 메소드
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");//출력 기능 WriteLine
+            //()안에 문장을 작성하고 작업이 끝난 뒤 띄어쓰기 작업 진행
+
+            //Console은 System.Console NET Framework 클래스
+            //콘솔에 대한 작업 관련 기능들이 들어있음
+        }
+    }
+}
