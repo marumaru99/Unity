701d804df74c14cdbfb26f83a71bd977173381a8
marumaru99 <102706594+marumaru99@users.noreply.github.com>
2023-03-14 21:39:48 +0900

    Add files via upload

---
+++ Class4.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class4
+    {
+        //C# operator(연산자)
+        //a+b - 연산자 : +, 항의 개수 : 2
+        //피연산자 : a, b
+
+        //연산자의 유형
+        //1. 산술
+        //2. 할당/대입
+        //3. 증감
+        //4. 논리
+        //5. 비교
+        //6. 비트
+        //7. 조건
+        
+       public static void Main(string[] args)
+        {
+            //1. 산술 연산자: 프로그래밍 코드를 통해 숫자에 대한 연산을 진행할 경우 사용하는 기호
+            //산술 연산자 사용 시 연산자의 우선 순위 파악해야 함
+            
+            //[우선 순위 기준]
+            //1. 항의 개수에 따라 결정 ex)a=5, a++ +7
+            //일반적으로는 단항 -> 이항 -> 삼항 순으로 우선순위를 가짐
+            //단 a+처럼 후위연산일 경우에는 연산이 끝나도 난 뒤 처리
+            //2. 산술 -> 비교 -> 논리 -> 대입 순으로 우선 순위 결정
+            //3. 괄호를 통해 우선순위 변경 가능
+            int x = 10; int y = 15; int z = 20;
+            int a = x + y + z; //45
+            int b = (x + y - z)*3; //15
+            int c = a - b % 3;             
+            Console.WriteLine(c);
+
+            //2. 할당/대입 연산자
+            //산술 연산자와 결합해 복합 대입 연산자로 많이 활용되며 값에 대한 적용을 진행
+            int money = 10000;
+            money += 3000; //money에 3000 더한 값을 대입(money = money+3000)
+            Console.WriteLine(money);
+            // +=, -=, *=, /=, %=, =
+
+            //3. 증감 연산자 
+            //값을 1증감시키거나 1을 감소시키는 연산자로 for문에서 증감식으로 많이 활용
+            //++, --
+            //후위연산 a++, a-- : 연산을 마무리한 뒤 항의 값을 1 증가, 감소시킴
+            //전위연산 ++a, --a : 항의 값을 1 증가, 감소시킨 뒤 연산을 진행
+            int j = 10;
+            int i = j-- + 5 - --j; //7
+            //10+5=15, j->9
+            //15-(j-1) -> 15-8 = 7
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+            //과제 : 계산 순서 작성
+            bool result = 4+5>2-1*7&&(12&3*2)>7||-2 != 2;
+            //A&&B는 A,B결과가 둘 다 true일 경우 true를 내보냄
+            //A||B는 둘 중 하나라도 true일 경우 true
+            //a!=b는 a,b가 서로 다르다는 뜻
+            //a&b는 a와 b를 비트 AND연산 = 논리연산
+
+            //4. 논리 연산자, 비교 연산자
+            //논리 연산자는 특정 식에 대한 결과를 판단할 때 사용하는 연산자로 주로 조건식을 세우고자 할때 사용(비교할 식이 2개 이상일 때 자주 사용)
+            //&& - AND연산
+            //|| - OR연산
+            //!A - A의 결과를 반대로 처리하는 NOT연산
+            //비교 연산자는 값에 대한 상대적인 크기 비교를 진행하는 연산자
+            
+            int atk = 30;
+            int level = 40;
+            string type = "melee";
+            //만약 공격력이 10 이상, 레벨이 30이하일 경우 입장 가능
+            if(atk>=10&&level<=30)
+            {
+                Console.WriteLine("입장 가능");
+            }
+            //해당 조건은 위의 조건을 만족하지 않은 상태이면 한번 더 체크 진행
+            else if(type == "melee")
+            {
+                Console.WriteLine("입장할 수 있습니다.");
+            }
+            else { Console.WriteLine("입장 불가능"); }
+        }
+    }
+}
---
+++ Class5.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//컬렉션(Collection) : C#에서는 데이터에 대한 저장을 진행할 때 특정 상황에
+//쓰기 적합한 자료구조를 컬랙션으로 제공
+
+//자료구조(Data Structure): 자료에 대한 저장, 검색, 추가, 삭제 등을 포함하는 개념
+
+//제네릭(Generic) : <>안에 타입을 작성해 해당 자료구조에 특정 데이터만 들어갈 수 있게 처리해주는 용도로 사용
+
+//List<T>
+//리스트: 데이터를 추가 삭제 검색 정렬 등을 진행할 수 있는 자료구조
+//데이터가 순서대로 저장되는 특징을 가짐
+
+//장점 : 배열과 많이 비교됨. 동적으로 크기 조절이 가능
+//단점 : 여분의 공간을 만들어주는 형태이기 때문에 배열보다는 작업이 느림
+//이는 리스트의 용량이 얼마나 필요한지를 판단할 수 있으며 해당 값에 대한 설정을 진행하는 것으로 극복가능(최적화)
+
+//자주 사용되는 문법
+//리스트명.Add(값); : 리스트에 해당 값을 추가함. 여러 번 작업할 경우 순서대로 추가
+//리스트명.Insert(인덱스 번호, 값); : 리스트이 인덱스 번호란에 값을 추가하는 기능
+//리스트명.GetRange(시작 범위, 끝 범위); : 범위 안에 있는 요소를 따로 추출
+//리스트명.Remove(값); : 리스트에 특정 값을 제거하는 기능. 같은 값이 존재할 경우 앞에 값이 제거됨
+//리스트명.Contains(값); : 해당 값을 리스트가 가지고 있는 지 확인하는 함수(True/False)
+
+//같이 활용되는 문법
+//리스트 내에 있는 요소를 가로로 출력하는 문법
+//foreach(자료형 변수명 in 리스트명)
+//{
+//    Console.WriteLine(변수명 + " ");
+//}
+
+namespace ConsoleApp1
+{
+    internal class Class5
+    {
+
+        public static void Main(string[] args)
+        {
+            List<string> item_list = new List<string>();
+            //스트링타입의 데이터를 관리하는 리스트 객체 item_list
+
+            item_list.Add("이쑤시개"); //추가
+            item_list.Add("휴지");
+            item_list.Add("물");
+            item_list.Add("손전등");
+            item_list.Add("밧줄");
+           // item_list.Remove("이쑤시개"); //제거
+
+            Console.WriteLine(item_list[0]); //자료구조 리스트는 동적으로 생성하고 삭제할 수 있는 가변 배열
+            //배열의 기능 인덱스를 가짐
+            //인덱스 : 배열에서 값의 위치를 찾을 수 있는 양의 정수 (0부터 배열의 길이 -1까지 주어짐)
+            Console.WriteLine($"리스트가 가지고 있는 원소의 개수는 {item_list.Count}");
+            Console.WriteLine($"리스트의 전체 용량은 {item_list.Capacity}");
+        }
+    }
+}
